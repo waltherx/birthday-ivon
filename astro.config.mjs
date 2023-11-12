87501aff8eff5ab42fea8f390c13978c67c5de46
waltherx <waltermj2012@gmail.com>
2023-11-12 01:31:26 -0400

    update lottie

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind()],
+  site: "https://ivon.vercel.app/",
+  integrations: [tailwind(), react()],
   vite: {
     ssr: {
       //external: ["svgo"],
-    },
-  },
+    }
+  }
 });
---
+++ src/components/Countdown/Wish.jsx
@@ -0,0 +1,10 @@
+
+const Wish = ({ name }) => {
+  return (
+    <div className='wish-message'>
+      HAPPY BIRTHDAY <span className='highlight'>{name.toUpperCase()}</span> !!!
+    </div>
+  );
+};
+
+export default Wish;
